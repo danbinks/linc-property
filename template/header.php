635e82c08e72a215cd1b4bd840a17e1977f4c279
Dan Binks <info@madloveapps.com>
2013-05-01 14:43:13 +0800

    basic framework

---
+++ template/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo $title; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
